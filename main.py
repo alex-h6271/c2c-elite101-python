3fbce765d0112639f6328d10874a41287219c37d
alex-h6271 <alexander.hernandez.0183@gmail.com>
2025-11-13 17:09:39 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ store_inventory = [
   {'prod_Id': 4327, 'type': 'Shoes', 'price': 100.0, 'total': 20},
   {'prod_Id': 3915, 'type': 'Tshirts', 'price': 43.5, 'total': 32},
   {'prod_Id': 2119, 'type': 'Pants', 'price': 34.0, 'total': 19},
-  {'prod_Id': 1194, 'type': 'Jumpers', 'price': 250.0, 'total': 5},  
+  {'prod_Id': 1194, 'type': 'Jumpers', 'price': 25.50, 'total': 5},  
   {'prod_Id': 1300, 'type': 'Blouse', 'price': 24.76, 'total': 3},
   {'prod_Id': 1118, 'type': 'Dress', 'price': 50.0, 'total': 10}, 
-  {'prod_Id': 1664, 'type': 'Suits', 'price': 250, 'total': 5}
+  {'prod_Id': 1664, 'type': 'Suits', 'price': 250.0, 'total': 5}
 ] 
 
 username = 'Manager123'
